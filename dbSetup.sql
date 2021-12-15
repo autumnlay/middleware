19d5739ccf0382b50af0366a8ab44c08f4bbee86
autumnlay <autumn.lay0@gmail.com>
2021-12-15 16:05:17 -0700

    init

--- dbSetup.sql
+++ dbSetup.sql
@@ -6,3 +6,25 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+-- this is just a helping file for extention to talk to scale grid
+--this is also where we can practice writing our MySQL code for our Repository to talk to
+CREATE TABLE IF NOT EXISTS castles(
+  id INT AUTO_INCREMENT NOT NULL primary key,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  cname TEXT NOT NULL,
+  clan TEXT NOT NULL,
+  clocation TEXT NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+DROP TABLE castles;
+INSERT INTO
+  castles(cname, clan, clocation, creatorId)
+VALUES
+  (
+    "Downton2",
+    "Abby2",
+    "England2",
+    "6199c3b1e8aa5014e38842f7"
+  );
